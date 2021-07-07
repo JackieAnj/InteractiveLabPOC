da1bfcedf96fa1b65a127eb4322539d3f2e0e998
Jackie <jackie.anj.liu@gmail.com>
2021-07-07 11:18:40 -0400

    add logic for part 2

--- InteractiveLab/Assets/Scripts/SystemState.cs
+++ InteractiveLab/Assets/Scripts/SystemState.cs
@@ -14,8 +14,10 @@ public class SystemState : MonoBehaviour
     public GameObject CondensationTrapTwo;
     public Text statusUI;
     public GameObject partOneText;
+    public GameObject partTwoText;
+    private Transform textContent;
     private TextMeshProUGUI content;
-
+    private int section = 1;
     private TwoWayValve[] twoWayValves;
     private ThreeWayValve[] threeWayValves;
     private CircleValve[] circleValves;
@@ -26,8 +28,27 @@ public class SystemState : MonoBehaviour
         threeWayValves = UnityEngine.Object.FindObjectsOfType<ThreeWayValve>();
         circleValves = UnityEngine.Object.FindObjectsOfType<CircleValve>();
         PRVs = UnityEngine.Object.FindObjectsOfType<PRVValve>();
+        textContent = partOneText.transform;
 
         startupCheck();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown("c")) {
+            if (section == 1) {
+                section = 2;
+                textContent = partOneText.transform;
+                partOneText.SetActive(false);
+                partTwoText.SetActive(true);
+                partTwo();
+            } else {
+                section = 1;
+                textContent = partTwoText.transform;
+                partOneText.SetActive(true);
+                partTwoText.SetActive(false);
+                partOne();
+            }
+        }
     }
 
     public void onChange() {
@@ -38,10 +59,12 @@ public class SystemState : MonoBehaviour
         }
 
         if (state > -1) {
-            partOne();
+            if (section == 1) {
+                partOne();
+            } else {
+                partTwo();
+            }
         }
-
-        updateStatus();
     }
 
     private bool startupCheck() {
@@ -92,43 +115,82 @@ public class SystemState : MonoBehaviour
     private void partOne() {
         if (checkPosition("V122", Position.left)) {
             state = 1;
-            statusUI.text = "step: " + state;
+            statusUI.text = "Part 1: step " + state;
             if (checkCircle("V121")) {
                 state = 2;
-                statusUI.text = "step: " + state;
+                statusUI.text = "Part 1: step " + state;
                 if (checkPosition("V131", Position.left)) {
                     state = 3;
-                    statusUI.text = "step: " + state;
-                        if (checkPosition("V119", Position.left) && checkPosition("V123", Position.left)) {
-                            state = 4;
-                            statusUI.text = "step: " + state;
-                            if (checkPosition("V124", Position.left) && checkOpen("V125")) {
-                                state = 5;
-                                statusUI.text = "step: " + state;
-                                if (checkCircle("V132")) {
-                                    state = 6;
-                                    statusUI.text = "step: " + state;
-                                    if (checkTurn("PRV10", 1)) {
-                                        state = 7;
-                                        statusUI.text = "step: " + state;
-                                        if (checkCircle("V111")) {
-                                            state = 8;
-                                            statusUI.text = "step: " + state;
-                                            if (checkTurn("PRV10", 3)) {
-                                                state = 9;
-                                                statusUI.text = "step: " + state;
-                                            }
+                    statusUI.text = "Part 1: step " + state;
+                    if (checkPosition("V119", Position.left) && checkPosition("V123", Position.left)) {
+                        state = 4;
+                        statusUI.text = "Part 1: step " + state;
+                        if (checkPosition("V124", Position.left) && checkOpen("V125")) {
+                            state = 5;
+                            statusUI.text = "Part 1: step " + state;
+                            if (checkCircle("V132")) {
+                                state = 6;
+                                statusUI.text = "Part 1: step " + state;
+                                if (checkTurn("PRV10", 1)) {
+                                    state = 7;
+                                    statusUI.text = "Part 1: step " + state;
+                                    if (checkCircle("V111")) {
+                                        state = 8;
+                                        statusUI.text = "Part 1: step " + state;
+                                        if (checkTurn("PRV10", 3)) {
+                                            state = 9;
+                                            statusUI.text = "Part 1: step " + state;
                                         }
                                     }
                                 }
                             }
                         }
+                    }
                 }
             }
         } else {
             state = 0;
             startupCheck();
         }
+
+        updateStatus();
+    }
+
+    private void partTwo() {
+        if (checkPosition("V122", Position.left)) {
+            state = 1;
+            statusUI.text = "Part 2: step " + state;
+            if (checkCircle("V121")) {
+                state = 2;
+                statusUI.text = "Part 2: step " + state;
+                if (checkPosition("V119", Position.left)) {
+                    state = 3;
+                    statusUI.text = "Part 2: step " + state;
+                    if (checkPosition("V123", Position.left)) {
+                        state = 4;
+                        statusUI.text = "Part 2: step " + state;
+                        if (checkCircle("V132")) {
+                            state = 5;
+                            statusUI.text = "Part 2: step " + state;
+                            if (checkTurn("PRV10", 1)) {
+                                state = 6;
+                                statusUI.text = "Part 2: step " + state;
+                                if (checkCircle("V111")) {
+                                    state = 7;
+                                    statusUI.text = "Part 2: step " + state;
+                                    if (checkTurn("PRV10", 3)) {
+                                        state = 8;
+                                        statusUI.text = "Part 2: step " + state;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        updateStatus();
     }
 
     // check if a two way valve is open given valve id
@@ -152,10 +214,9 @@ public class SystemState : MonoBehaviour
     }
 
     private void updateStatus() {
-        Transform partOneTextContent = partOneText.transform;
         int index = 0;
 
-        foreach(Transform instruction in partOneTextContent) {
+        foreach(Transform instruction in textContent) {
             index++;
             content = instruction.GetComponent<TextMeshProUGUI>();
 
