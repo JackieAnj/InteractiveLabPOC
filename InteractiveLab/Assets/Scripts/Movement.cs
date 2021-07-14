ae01dd4286870714eecd4d0d54467a3d9239035a
Jackie <jackie.anj.liu@gmail.com>
2021-07-14 11:30:29 -0400

    add control scheme change

--- InteractiveLab/Assets/Scripts/Movement.cs
+++ InteractiveLab/Assets/Scripts/Movement.cs
@@ -6,17 +6,46 @@ public class Movement : MonoBehaviour
 {
 
     public CharacterController controller;
-
+    public GameObject HX;
+    public GameObject HXPivot;
+    private bool freeControl = true;
     public float speed = 50f;
+    Vector3 playerPosition;
+    Vector3 HXPosition;
+
+    void Start() {
+        playerPosition = transform.position;
+        HXPosition = HX.transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown("left shift")) {
+            freeControl = !freeControl;
+            if (freeControl) {
+                HX.transform.position = HXPosition;
+                HX.transform.rotation = Quaternion.identity;
+            } else {
+                transform.position = playerPosition;
+            }
+        }
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        if (freeControl) {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        controller.Move(move * speed * Time.deltaTime);
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+        } else {
+            if (Input.GetKey("a")) {
+                HX.transform.RotateAround(HXPivot.transform.position, Vector3.up, speed * Time.deltaTime);
+            }
+            
+            if (Input.GetKey("d")) {
+                HX.transform.RotateAround(HXPivot.transform.position, Vector3.up, -speed * Time.deltaTime);
+            }
+        }
     }
 }
