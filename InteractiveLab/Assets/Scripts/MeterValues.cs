49feb99a0126372daa194310838dae472a9f83cd
Jackie Liu <jackie.anj.liu@gmail.com>
2022-03-28 19:45:31 -0400

    some bug fixes and improvements

--- InteractiveLab/Assets/Scripts/MeterValues.cs
+++ InteractiveLab/Assets/Scripts/MeterValues.cs
@@ -19,22 +19,23 @@ public class MeterValues : MonoBehaviour
     public Text[] meterList;
 
     // Start is called before the first frame update
-    void Start()
+    public MeterValues()
     {
-        FIT16.text = "60.0";
-        PI16.text = "4.4";
-        TI16.text = "105.7";
-        FT17.text = "3.3";
-        FT18.text = "5.6";
-        TE42.text = "4.9";
-        TE43.text = "32.5";
-        TE44.text = "6.3";
-        TE45.text = "18.5";
-        TE46.text = "9.4";
-        meterList = new Text[] {FIT16, PI16, TI16, FT17, FT18, TE42, TE43, TE44, TE45, TE46};
+        // FIT16.text = "60.0";
+        // PI16.text = "4.4";
+        // TI16.text = "105.7";
+        // FT17.text = "3.3";
+        // FT18.text = "5.6";
+        // TE42.text = "4.9";
+        // TE43.text = "32.5";
+        // TE44.text = "6.3";
+        // TE45.text = "18.5";
+        // TE46.text = "9.4";
+        // meterList = new Text[] {FIT16, PI16, TI16, FT17, FT18, TE42, TE43, TE44, TE45, TE46};
     }
 
     public void changeValue(int id, string val) {
+        Debug.Log(meterList);
         Debug.Log(meterList[id].text);
         meterList[id].text = val;
     }
