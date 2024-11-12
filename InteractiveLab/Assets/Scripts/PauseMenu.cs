ed545b822c66a32c9c56c611bb0877aae93033fa
MichaelPCTower <xywang02@gmail.com>
2024-11-12 11:44:42 -0500

    fix: it turns out for VR, Canvas appears twice - one is VR Canvas (Camera) and the other is VR Canvas (World Space). This caused the button press to be registered twice in a row. I disabled the PauseMenu for the VR Canvas (Camera)

--- InteractiveLab/Assets/Scripts/PauseMenu.cs
+++ InteractiveLab/Assets/Scripts/PauseMenu.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using System.Linq;
+using UnityEngine.Rendering;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -10,12 +12,12 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseMenuUI;
     
     [SerializeField]
-    private XRNode xrNode = XRNode.LeftHand;
+    private XRNode xrNode = XRNode.RightHand;
     private List<InputDevice> devices = new List<InputDevice>();
     private InputDevice device;
     private bool wasPressed = false;
-
-    void getDevice()
+    
+    void GetDevice()
     {
         InputDevices.GetDevicesAtXRNode(xrNode, devices);
         device = devices.FirstOrDefault();
@@ -24,9 +26,12 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // it turns out for VR, Canvas appears twice - one is VR Canvas (Camera) and the other is VR Canvas (World
+        // Space). This caused the button press to be registered twice in a row.
+        
         if (!device.isValid)
         {
-            getDevice();
+            GetDevice();
         }
 
         bool isPressed = device.TryGetFeatureValue(CommonUsages.primaryButton, out bool pressed) && pressed;
@@ -51,6 +56,8 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         paused = false;
         Cursor.lockState = CursorLockMode.Locked;
+        
+        // Debug.Log($"Pause menu is inactive, Paused is {paused}");
     }
 
     void Pause() {
@@ -58,6 +65,8 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0f;
         paused = true;
         Cursor.lockState = CursorLockMode.Confined;
+        
+        // Debug.Log($"Pause menu is active, Paused is {paused}");
     }
 
     public void Exit() {
